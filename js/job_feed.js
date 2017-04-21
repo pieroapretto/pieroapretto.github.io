12e592a20f79d458172ef80918e1f0098eb8bbb5
ASTPMeetup <pretto417@gmail.com>
2017-04-21 15:45:53 -0500

    update job feed platform

--- js/job_feed.js
+++ js/job_feed.js
@@ -1,16 +1,27 @@
 var app = angular.module('job_feed',[]);
 var date = new Date();
-var feedUrl = "https://openws.herokuapp.com/jobs?apiKey=8fa0e46f0361117d65d91d6032391324";
+const feedUrl = "https://openws.herokuapp.com/jobs";
+const urlKey = "?apiKey=8fa0e46f0361117d65d91d6032391324";
 
 app.controller('FeedCtrl', function($scope, $http){
   $scope.jobListings = [];
 
-  $http.get(feedUrl)
+  $scope.DeleteJob = function(id) {
+    $http.delete(feedUrl + '/' + id + urlKey)
+      .then(function success(response){
+        var updatedList = $scope.jobListings.filter(job => job._id !== id);
+        $scope.jobListings = updatedList;
+      }, function error(response){
+          console.log(response);
+    });
+  }
+
+  $http.get(feedUrl + urlKey)
     .then(function success(response) {
       var feed = response.data;
 
       feed.forEach(function(job){
-        job.date = job.created_at.slice(0,9).replace(/-/g, "");
+        job.date = job.created_at.slice(0,10).replace(/-/g, "");
         job.year = job.date.substring(0,4);
         job.month = job.date.substring(4,6);
         job.day = job.date.substring(6);
